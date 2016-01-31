764b06ba19ddd256f13c0511c8417e15356c64ce
= <jtstodola@gmail.com>
2016-01-30 19:03:26 -0600

    Created test to keep car from decelerating below 0

--- test/CarTest.java
+++ test/CarTest.java
@@ -40,7 +40,12 @@ public class CarTest {
 		assertThat(car1.getSpeed(), is(50));
 	}
 	
-	// Create and pass test for no deceleration below 0.
-
+	@Test
+	public void cannotDecelerateBelowZero() {
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(30);
+		car1.decelerate(40);
+		assertThat(car1.getSpeed(), is(0));
+	}
 	
 }
